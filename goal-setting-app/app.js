47b21a7ed1c4eb43c4d076d8a46f9b375cb21cbe
Daniel Kimball <danielkimball@Daniels-MacBook-Pro-4.local>
2019-12-16 21:55:30 -0700

    Create goal tracker func

--- goal-setting-app/app.js
+++ goal-setting-app/app.js
@@ -3,3 +3,7 @@ const goals = require('goals_tracker')
 console.log("Hello Dan!")
 
 console.log("MD my man whats good?")
+
+const goalTracker = (goalName) => {
+    console.log(`${goalName} was just created`)
+}
